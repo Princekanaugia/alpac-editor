2af354a9cb874d4efa57a127dd23d4cf299ccf65
Prince Kanaugia <princekanaugia@gmail.com>
2024-03-03 22:32:20 +0530

    added button and png file for alpaca image

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,26 @@ import './App.css'
 
 function App() {
 
-  return <>
-    <h1 className='m-2 p-4'>
+  return <div className='container p-4 mx-auto my-4 bg-slate-200 max-w-2xl'>
+    <h1 className='font-serif font-black text-3xl pb-3'>
       ALPACA GENERATOR
     </h1>
-    <div className='flex flex-row content-center m-auto'>
+    <div className='flex flex-row flex-wrap content-center m-auto font-mono font-bold container'>
       <div className='basis-1/2'>
         image
       </div>
       <div className='basis-1/2'>
         <div className='block'>
           <div className=''>ACCESSORIZE THE ALPACA&apos;S</div>
-          <div className='grid grid-cols-6 gap-1 p-4 mx-2 my-1'>
-            <div className='col-span-2 content-center'><button className='border-blue-600 rounded-3xl border-2 px-4 text-blue-600'>Hairs</button></div>
-            <div className='col-span-2 content-center'><button className='border-blue-600 rounded-3xl border-2 px-4 text-blue-600'>Ears</button></div>
-            <div className='col-span-2 content-center'><button className='border-blue-600 rounded-3xl border-2 px-4 text-blue-600'>Eyes</button></div>
-            <div className='col-span-2 content-center'><button className='border-blue-600 rounded-3xl border-2 px-4 text-blue-600'>Mouth</button></div>
-            <div className='col-span-2 content-center'><button className='border-blue-600 rounded-3xl border-2 px-4 text-blue-600'>Neck</button></div>
-            <div className='col-span-2 content-center'><button className='border-blue-600 rounded-3xl border-2 px-4 text-blue-600'>Leg</button></div>
-            <div className='col-span-3 content-center'><button className='border-blue-600 rounded-3xl border-2 px-4 text-blue-600'>Accessories</button></div>
-            <div className='col-span-3 content-center'><button className='border-blue-600 rounded-3xl border-2 px-4 text-blue-600'>Background</button></div>
+          <div className='grid grid-cols-6 gap-1 py-3'>
+            <div className='col-span-2 content-center'><button className='btn-primary'>Hairs</button></div>
+            <div className='col-span-2 content-center'><button className='btn-primary'>Ears</button></div>
+            <div className='col-span-2 content-center'><button className='btn-primary'>Eyes</button></div>
+            <div className='col-span-2 content-center'><button className='btn-primary'>Mouth</button></div>
+            <div className='col-span-2 content-center'><button className='btn-primary'>Neck</button></div>
+            <div className='col-span-2 content-center'><button className='btn-primary'>Leg</button></div>
+            <div className='col-span-3 content-center'><button className='btn-primary'>Accessories</button></div>
+            <div className='col-span-3 content-center'><button className='btn-primary'>Background</button></div>
           </div>
         </div>
         <div>
@@ -33,7 +33,7 @@ function App() {
         </div>
       </div>
     </div>
-    </>
+    </div>
 }
 
 export default App
